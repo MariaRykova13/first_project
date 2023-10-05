e67795990634b9e5401821650d8eb2f2386c6e17
Maria Rykova <marija.rykova@rambler.ru>
2023-10-05 20:32:40 +0300

    commit 05.10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,32 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome world!");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        WebElement userNameInput = driver.findElement(By.xpath("//input[@id='userName']"));
+        WebElement userEmailInput = driver.findElement(By.xpath("//input[@id='userEmail']"));
+        WebElement currentAddrInput = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
+        WebElement permamentAddrInput = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
+        userNameInput.sendKeys("my login");
+        userEmailInput.sendKeys("1@ff.ru");
+        currentAddrInput.sendKeys("gg");
+        permamentAddrInput.sendKeys("ss");
+        WebElement buttonSubmit = driver.findElement(By.xpath("//button[@id='submit']"));
+        buttonSubmit.click();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
